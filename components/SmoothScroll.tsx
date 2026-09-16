02bfa9fc4f1789dbb8977618c0cdc3fc25d3b12e
sheddy-cyber <idikechinaza@gmail.com>
2026-09-16 23:50:33 +0100

    Fix Lenis API compatibility - update to current Lenis options
    - Replace deprecated direction with orientation
    - Replace deprecated gestureDirection with gestureOrientation
    - Replace deprecated smooth with smoothWheel
    - Replace deprecated mouseMultiplier with wheelMultiplier
    - Remove deprecated smoothTouch and touchMultiplier options
    - Update to work with current Lenis version API

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -8,12 +8,10 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
+      orientation: "vertical",
+      gestureOrientation: "vertical",
+      smoothWheel: true,
+      wheelMultiplier: 1,
     });
 
     function raf(time: number) {
